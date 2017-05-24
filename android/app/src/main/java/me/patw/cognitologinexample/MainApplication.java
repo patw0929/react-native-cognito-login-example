a3f66b46681d640815ecc6d8c723796fcbdc532f
Patrick <patw.hi@gmail.com>
2017-05-24 12:41:30 +0800

    Linked the react-native-google-signin

--- android/app/src/main/java/me/patw/cognitologinexample/MainApplication.java
+++ android/app/src/main/java/me/patw/cognitologinexample/MainApplication.java
@@ -3,6 +3,7 @@ package me.patw.cognitologinexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.amazonaws.reactnative.core.AWSRNCorePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
           new AWSRNCorePackage(),
           new FacebookLoginPackage()
       );
